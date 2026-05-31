c7750042d9ccd1c0b973568c6950e332a3ba40d4
Justice-falcon <2425402173@live.gctu.edu.gh>
2026-05-31 10:54:23 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 2
+#
+# Name: Justice Nangua
+# STUDENT ID: 2425402173
+# Email: 2425402173@live.gctu.edu.gh
+# Grader: Augustus
+#
+# On my honor, Justice Nangua, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Printing my initials
 
-
+    print()
+    print('...JNJ')
+    print()
+    print('...JJJJJJJJJJJJ........NN........NN........JJJJJJJJJJJJ.....')
+    print('...JJJJJJJJJJJJ........NNN.......NN........JJJJJJJJJJJJ.....')
+    print('........JJ.............NNNN......NN.............JJ..........')
+    print('........JJ.............NN.NN.....NN.............JJ..........')
+    print('........JJ.............NN..NN....NN.............JJ..........')
+    print('........JJ.............NN...NN...NN.............JJ..........')
+    print('........JJ.............NN....NN..NN.............JJ..........')
+    print('...JJ...JJ.............NN.....NN.NN........JJ...JJ..........')
+    print('...JJJJJJJ........**...NN......NNNN...**...JJJJJJJ........**')
+    print('....JJJJJ.........**...NN.......NNN...**....JJJJJ.........**')
+    print()
 
 main()
